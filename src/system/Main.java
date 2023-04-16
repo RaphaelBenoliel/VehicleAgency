9a98c092cfdd73933fc927e224552e21cbd20a8c
Raphael Benoliel <raphael2gb@gmail.com>
2023-04-16 21:29:50 +0300

    Building the main..

--- src/system/Main.java
+++ src/system/Main.java
@@ -3,23 +3,88 @@ package system;
 import vehicle.*;
 
 import java.util.Scanner;
-enum option {
-    BuyVehicle, TestVehicle, Reset_ALL_KM, ChangingFlag, Exit
-}
-public class Main {
 
+public class Main {
+    private enum Option {
+        BuyVehicle, TestVehicle, Reset_ALL_KM, ChangingFlag, Exit
+    }
     public static void main(String[] args) {
 
         Vehicle[] Agency = null;
-        for(int i=0; i<5; i++){
-            Agency = addVehicle(Agency);
+        Vehicle temp = null;
+        for (int i = 0; i < 5; i++) {
+           temp = createVehicle(Agency);
+           if (temp == null){
+               System.out.println("Invalid vehicle type. Please try again.");
+               i--;
+               continue;
+           }
+           else{
+               Agency = addVehicle(Agency, temp);
+           }
         }
         for (Vehicle vehicle : Agency) {
             System.out.println(vehicle);
         }
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Please choose an option:");
+            System.out.println("1. Add a vehicle");
+            System.out.println("2. Buy a vehicle");
+            System.out.println("3. Test drive a vehicle");
+            System.out.println("4. Reset distance traveled for all vehicles");
+            System.out.println("5. Change flag for all vessels");
+            System.out.println("6. Exit");
+
+            Option option = scanner.nextInt() == 1 ? Option.BuyVehicle :
+                    scanner.nextInt() == 2 ? Option.TestVehicle :
+                            scanner.nextInt() == 3 ? Option.Reset_ALL_KM :
+                                    scanner.nextInt() == 4 ? Option.ChangingFlag :
+                                            Option.Exit;
+            switch (option) {
+                case BuyVehicle:
+                    System.out.println("BuyVehicle");
+
+
+                    break;
+                case TestVehicle:
+                    System.out.println("TestVehicle");
+                    break;
+                case Reset_ALL_KM:
+                    System.out.println("Reset_ALL_KM");
+                    break;
+                case ChangingFlag:
+                    System.out.println("ChangingFlag");
+                    break;
+                case Exit:
+                    System.out.println("Goodbye!");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid option. Please choose again.");
+            }
+        }
 
     }
-    public static Vehicle[] addVehicle(Vehicle[] Agency){
+    public static Vehicle[] addVehicle(Vehicle[] Agency, Vehicle vehicle) {
+        if (Agency == null) {
+            Agency = new Vehicle[1];
+            Agency[0] = vehicle;
+        }
+        else
+        {
+            Vehicle[] temp = new Vehicle[Agency.length + 1];
+            for (int i = 0; i < Agency.length; i++) {
+                temp[i] = Agency[i];
+            }
+            temp[temp.length - 1] = vehicle;
+            Agency = temp;
+        }
+        return Agency;
+    }
+    public static Vehicle createVehicle(Vehicle[] Agency) {
         Vehicle vehicle = null;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please choose a vehicle type:");
@@ -31,7 +96,8 @@ public class Main {
         int type = scanner.nextInt();
         scanner.nextLine(); // consume the new line character
 
-        switch(type){
+
+        switch (type) {
             case 1:
                 System.out.println("Please enter the model of the Jeep:");
                 String model = scanner.nextLine();
@@ -62,18 +128,16 @@ public class Main {
                 break;
             case 4:
                 vehicle = new GameGlider();
+                break;
+            default:
+                System.out.println("Invalid option. Please choose again.");
         }
-        if(Agency == null) {
-            Agency = new Vehicle[1];
-            Agency[0] = vehicle;
-        }
-        else {
-            Vehicle[] temp = new Vehicle[Agency.length + 1];
-            for(int i=0; i<Agency.length; i++)
-                temp[i] = Agency[i];
-            temp[Agency.length] = vehicle;
-            Agency = temp;
-        }
-        return Agency;
+        return vehicle;
+    }
+    public static void buyVehicle (Vehicle[]Agency){
+            for (Vehicle vehicle : Agency)
+                System.out.println(vehicle);
+
+
     }
 }
